eed5e41a15694f8f7c2b37bdd448cd21bc7b5bf3
sobha <sobha@DESKTOP-CMS5M0M>
2021-02-05 22:56:16 +0530

    added delete url in controller

--- src/main/java/com/sobhan/io/bookmanagement/controller/Controller.java
+++ src/main/java/com/sobhan/io/bookmanagement/controller/Controller.java
@@ -3,9 +3,13 @@ package com.sobhan.io.bookmanagement.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,18 +28,36 @@ public class Controller {
 
 		return this.booksserv.getBooks();
 	}
-	
-	//GET single book as per id
+
+	// GET single book as per id
 	@GetMapping("/books/{bookId}")
 	public Books getBook(@PathVariable String bookId) {
 
 		return this.booksserv.getBooks(Long.parseLong(bookId));
 	}
-	
-	//ADD book to list
+
+	// ADD book
 	@PostMapping("/books")
 	public Books addBook(@RequestBody Books books) {
-		
+
 		return this.booksserv.addBooks(books);
 	}
+
+	// update books using put
+	@PutMapping("/books")
+	public Books updateBook(@RequestBody Books books) {
+		return this.booksserv.updateBooks(books);
+	}
+
+	// delete books
+	@DeleteMapping("/books/{bookId")
+	public ResponseEntity<HttpsStatus> deleteBooks(@PathVariable String BookId){
+		try {
+			this.booksserv.deleteBooks(Long.parseLong(bookId));
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+}
+
 }
