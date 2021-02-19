bb3262b532289b1021d68295eb3d4ccfdbcaffe5
sobha <sobha@DESKTOP-PUDUEPK>
2021-02-19 17:54:02 +0530

    comment

--- src/main/java/com/sobhan/io/bookmanagement/services/BooksServiceimpl.java
+++ src/main/java/com/sobhan/io/bookmanagement/services/BooksServiceimpl.java
@@ -10,20 +10,21 @@ import org.springframework.stereotype.Service;
 import com.sobhan.io.bookmanagement.dao.BooksDao;
 import com.sobhan.io.bookmanagement.entities.Books;
 @SuppressWarnings("unused")
+
 @Service
 public class BooksServiceimpl implements BooksService {
 	
 	@Autowired
 	private BooksDao booksDao;
 	
-											//	List<Books> list;
+																							//	List<Books> list;
 	public BooksServiceimpl() {
 		
 		
-											//		list = new ArrayList<>();
-											//		list.add(new Books(145,"Java core Course","This course contains basics of java"));
-											//		list.add(new Books(4343, "spring boot course","basics of spring boot "));
-											//		list.add(new Books(1245,"python basics","Basics of python"));
+																							//		list = new ArrayList<>();
+																							//		list.add(new Books(145,"Java core Course","This course contains basics of java"));
+																							//		list.add(new Books(4343, "spring boot course","basics of spring boot "));
+																							//		list.add(new Books(1245,"python basics","Basics of python"));
 	}
 	
 	
@@ -32,7 +33,7 @@ public class BooksServiceimpl implements BooksService {
 	//GET all books
 	public List<Books>getBooks() {
 		return booksDao.findAll();
-											//		return list;
+																							//		return list;
 	}
 
 
@@ -41,18 +42,19 @@ public class BooksServiceimpl implements BooksService {
 	//GET single book by bookId
 	public Books getBook(long bookId) {
 		return booksDao.getOne(bookId);
+
 	
-											//		Books c=null;
-											//		for(Books books:list)
-											//		{
-											//			if(books.getId()==bookId)
-											//			{
-											//				c = books;
-											//				break;
-											//			}
-											//		}
-											//		return c;
-											//	}
+																							//		Books c=null;
+																							//		for(Books books:list)
+																							//		{
+																							//			if(books.getId()==bookId)
+																							//			{
+																							//				c = books;
+																							//				break;
+																							//			}
+																							//		}
+																							//		return c;
+																							//	}
 	}
 
 
@@ -61,23 +63,23 @@ public class BooksServiceimpl implements BooksService {
 	public Books addBook(Books books) {
 		booksDao.save(books);
 		return books;
-											//		list.add(books);
-											//		return books;
-	}
+																							//		list.add(books);
+																							//		return books;
+	}				
 	
 	@Override
 	//PUT request to update a book
 	public Books updateBook(Books books) {
 		booksDao.save(books);
 		return books;
-											//		list.forEach(e-> {
-											//			if (e.getId()== books.getId()) {
-											//				e.setTitle(books.getTitle());
-											//				e.setDescription((books.getDescription());
+																							//		list.forEach(e-> {
+																							//			if (e.getId()== books.getId()) {
+																							//				e.setTitle(books.getTitle());
+																							//				e.setDescription((books.getDescription());
 				
-											//			}
-											//		});
-											//				return books;
+																							//			}
+																							//		});
+																							//				return books;
 				
 	}
 	
@@ -86,7 +88,7 @@ public class BooksServiceimpl implements BooksService {
 	public void deleteBook(long parseLong) {
 		Books entity = booksDao.getOne(parseLong);
 		booksDao.delete(entity);
-											//		list = this.list.stream().filter(e->e.getId()!=parselong).collect(Collectors.toList());
+																							//		list = this.list.stream().filter(e->e.getId()!=parseLong).collect(Collectors.toList());
 	}
 
 }
